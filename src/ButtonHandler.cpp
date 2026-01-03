ba594f2dd4aecbd5d64563d9b2995a382b36683b
KubikDezimeter <KubikDezimeter@gmail.com>
2026-01-03 20:31:45 +0100

    Add debouncing to ButtonHandler

--- src/ButtonHandler.cpp
+++ src/ButtonHandler.cpp
@@ -11,10 +11,50 @@ ButtonHandler& ButtonHandler::getButtonHandler() {
     return instance;
 }
 
-bool ButtonHandler::get_button_l() {
+bool ButtonHandler::get_button_state_l() {
     return (PINC & _BV(PC7)) == 0;
 }
 
-bool ButtonHandler::get_button_r() {
+bool ButtonHandler::get_button_state_r() {
     return (PINC & _BV(PC3)) == 0;
+}
+
+bool ButtonHandler::update_button_press_l(uint32_t current_time) {
+    bool button_pressed = false;
+
+    bool curr_button_state = get_button_state_l();
+
+    if (curr_button_state && !prev_button_l && current_time > button_l_cooldown) {
+        button_l_cooldown = current_time + 20;
+        button_pressed = true;
+    }
+    
+    // Set cooldown after button release to ignore bounces
+    if (!curr_button_state && prev_button_l) {
+        button_l_cooldown = current_time + 20;
+    }
+
+    prev_button_l = curr_button_state;
+
+    return button_pressed;
+}
+
+bool ButtonHandler::update_button_press_r(uint32_t current_time) {
+    bool button_pressed = false;
+
+    bool curr_button_state = get_button_state_r();
+
+    if (curr_button_state && !prev_button_r && current_time > button_r_cooldown) {
+        button_r_cooldown = current_time + 20;
+        button_pressed = true;
+    }
+    
+    // Set cooldown after button release to ignore bounces
+    if (!curr_button_state && prev_button_r) {
+        button_r_cooldown = current_time + 20;
+    }
+
+    prev_button_r = curr_button_state;
+
+    return button_pressed;
 }
--- src/ButtonHandler.h
+++ src/ButtonHandler.h
@@ -11,11 +11,20 @@ class ButtonHandler {
         ButtonHandler(ButtonHandler const&) = delete;
         void operator=(ButtonHandler const&) = delete;
 
-        static bool get_button_l();
-        static bool get_button_r();
+        static bool get_button_state_l();
+        static bool get_button_state_r();
+
+        bool update_button_press_l(uint32_t current_time);
+        bool update_button_press_r(uint32_t current_time);
 
     private:
         ButtonHandler();
+
+        bool prev_button_l {false};
+        bool prev_button_r {false};
+
+        uint32_t button_l_cooldown {0};
+        uint32_t button_r_cooldown {0};
 };
 
 #endif
