87cb62014b9c86517c80c6b7df5ebb45baee2be2
KubikDezimeter <KubikDezimeter@gmail.com>
2026-01-09 11:52:30 +0100

    feat: allow appending to Animation

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,7 +1,25 @@
 #include "Animation.h"
 
+Animation::Animation(uint8_t* const columns, const uint8_t col_count, const uint8_t capacity) :
+    columns {columns},
+    col_count {col_count},
+    capacity {capacity != 0 ? capacity : col_count},
+    offset {0}
+{};
+
+uint8_t Animation::getOffset() const {
+    return offset;
+}
+
 void Animation::setOffset(uint8_t offset) {
     offset = offset;
+}
+
+void Animation::append(const uint8_t* columns, const uint8_t col_count) {
+    for (uint8_t i = 0; i < col_count; ++i) {
+        this->columns[this->col_count + i] = columns[i];
+    }
+    this->col_count += col_count;
 }
 
 bool Animation::isFinished() const {
--- src/Animation.h
+++ src/Animation.h
@@ -1,12 +1,15 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
+#include <stdint.h>
 #include "Frame.h"
 
 class Animation {
     public:
-        Animation(const uint8_t* columns, const uint8_t col_count) : columns(columns), col_count(col_count), offset(0) {};
+        Animation(uint8_t* const columns, const uint8_t col_count, const uint8_t capacity=0);
+        uint8_t getOffset() const;
         void setOffset(uint8_t offset);
+        void append(const uint8_t* columns, const uint8_t col_count);
         bool isFinished() const;
         Frame render(bool loop=false);
 
@@ -14,8 +17,9 @@ class Animation {
         /*
          * MSB is the top pixel, LSB is the bottom pixel. Columns are ordered from left to right.
          */
-        const uint8_t* columns;
-        const uint8_t col_count;
+        uint8_t* const columns;
+        uint8_t col_count;
+        const uint8_t capacity;
         uint8_t offset;
 };
 
